9241386cddd681ba6780758f869456a2a7705cca
SageTripp <zhangst0720@163.com>
2017-09-07 00:34:15 +0800

    完成侧滑菜单的调用

---
+++ app/src/main/java/com/st/programerutils/MyApp.kt
@@ -0,0 +1,13 @@
+package com.st.programerutils
+
+import com.qihoo360.replugin.RePluginApplication
+
+/**
+ * Created by sage-tripp on 17-9-5.
+ * 程序入口
+ */
+class MyApp: RePluginApplication() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
